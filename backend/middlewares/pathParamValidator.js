ac11676265cc0478e716cb57f8b752fe3b1b9f93
Lazar Dilov <ldilov@isimarkets.com>
2022-01-29 17:53:40 +0200

    Deps + params validator

--- backend/middlewares/pathParamValidator.js
+++ backend/middlewares/pathParamValidator.js
@@ -2,13 +2,13 @@ import mongoose from 'mongoose';
 
 import InvalidPathParamError from '../exceptions/InvalidPathParamError.js';
 
-const PATH_PARAM_REGEX = /^\/api\/(?<controller>\w+)\/(?<pathParam>\w+)$/;
-
+const PATH_PARAM_REGEX = /^\/api\/(?<controller>[a-z]+)\/(?<pathParam>\w+)$/;
 
 function PathParamValidator(req, res, next) {
   const match = req.url.match(PATH_PARAM_REGEX);
   if (match) {
     const {controller, pathParam} = match.groups;
+
     if(!mongoose.Types.ObjectId.isValid(pathParam)){
       throw new InvalidPathParamError(pathParam, `Invalid id for ${ controller }!`, 400)
     }
