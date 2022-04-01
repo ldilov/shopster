690be30b2d511cf770d01e4ffd5d4c247aa25f34
Lazar Dilov <ldilov@isimarkets.com>
2022-04-02 01:09:01 +0300

    Typecasting to number

--- frontend/src/redux/cart/cart.selectors.js
+++ frontend/src/redux/cart/cart.selectors.js
@@ -6,9 +6,9 @@ const selectCartState = (state) => state.cart;
 export const selectCartItems = createSelector([selectCartState], (cart) => cart.items);
 
 export const selectCartItemsCount = createSelector([selectCartItems], (items) => {
-  return items.length > 0 ? items.reduce((accum, cartItem) => accum + cartItem.quantity, DEFAULT_ITEMS_COUNT) : DEFAULT_ITEMS_COUNT;
+  return items.length > 0 ? items.reduce((accum, cartItem) => Number(accum) + Number(cartItem.quantity), DEFAULT_ITEMS_COUNT) : DEFAULT_ITEMS_COUNT;
 });
 
 export const selectCartTotalPrice = createSelector([selectCartItems], (items) => {
-  return items.length > 0 ? items.reduce((accum, value) => accum + value.quantity * value.price, 0) : DEFAULT_ITEMS_TOTAL_PRICE;
+  return items.length > 0 ? items.reduce((accum, value) => Number(accum) + Number(value.quantity) * Number(value.price), 0) : DEFAULT_ITEMS_TOTAL_PRICE;
 });
